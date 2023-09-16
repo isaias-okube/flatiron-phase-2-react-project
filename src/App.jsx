b8243810cbfd0ae2aafc8341622eee80acd93d04
ISAIAS <issuyep@yahoo.com>
2023-09-15 23:19:49 -0600

    created main and footer jsx files

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header />
+      <Main />
+      <Footer />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
