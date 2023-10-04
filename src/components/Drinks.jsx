842cda91401a09c1a961632ff87be97fa0c62cbd
ISAIAS <issuyep@yahoo.com>
2023-10-04 13:37:22 -0600

    add table content

--- src/components/Drinks.jsx
+++ src/components/Drinks.jsx
@@ -2,27 +2,48 @@ import React from 'react'
 import { useEffect } from 'react'
 
 function Drinks() {
+
     useEffect(() => {
-        fetch('http://localhost:3002/drinks')
-        .then(response => response.json())
-        .then(data => console.log(data))
-    }, []
-    )
+        const fetchDrinks = async () => {
+            const url = 'http://localhost:3002/drinks'
+        try {
+            const response = await fetch(url)
+            const data = await response.json()
+            console.log(data)
+        } catch (err) {
+            console.log(err)
+            return null;
+        }
+    }
+    fetchDrinks()
+    }
+    , [])
 
     return (
         <>
-        <h2>Drinks</h2>
+        <h1>Drinks</h1>
         <table>
             <thead>
-             <tr>
-
-             </tr>
+                <tr>
+                    <th>Drink</th>
+                    <th>category</th>
+                    <th>Ingrediant</th>
+                    <th>ID</th>
+                    </tr>
             </thead>
             <tbody>
-               
+                {/*Drinks.map((drink) => {
+                    return (
+                        <tr key={drink.idDrink}>
+                            <td>{drink.strDrink}</td>
+                            <td>{drink.strCategory}</td>
+                            <td>{drink.strIngredient1}</td>
+                            <td>{drink.idDrink}</td>
+                        </tr>
+                    )
+                })*/}
             </tbody>
         </table>
-
         </>
     )
     }
